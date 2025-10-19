89731b3ba9a5e01ff247c509b842193989506cc7
DennysGongoraMera <dennysgmera@gmail.com>
2025-10-19 17:25:22 -0500

    login y register terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, computed } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
   protected readonly title = signal('my-app-dsi6');
+
+  constructor(private router: Router) {}
+
+  // Esta señal verifica si estamos en login o register
+  isAuthPage = computed(() => {
+    const currentUrl = this.router.url;
+    return currentUrl === '/login' || currentUrl === '/register';
+  });
 }
